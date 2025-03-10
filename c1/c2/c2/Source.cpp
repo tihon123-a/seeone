461a608bffb5925b3a58fc82f1441842d2004c21
tihon123-a <tihonkorol3@gmail.com>
2025-03-10 18:38:03 +0300

    Add files via upload

---
+++ c1/c2/c2/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int a = 2;
+	int b;
+	std::cout << a;
+	std::cout << "hello world!\n";
+	std::cout << "input value a:";
+	std::cin >> a;
+	std::cout << "\nInput value b: ";
+	std::cin >> b;
+	std::cout << "you imput" << a * b;
+
+	return 0;
+}
